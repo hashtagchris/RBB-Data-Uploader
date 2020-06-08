17bedafa221323ce660cbbe9b5dff62b87be00d8
Jayson J. Phillips <jjp@breakpointt.studio>
2020-06-07 21:45:29 -0700

    feat: 🎸 Dineblack csv data parsing support
    Dineblack csv mostly matches the current Airtable schema. Has matching
    file in converters folder.

---
+++ src/csv/converters/dineblack.js
@@ -0,0 +1,19 @@
+const { schema } = require('../lib/airtable');
+// we dont need a mapping here as the dataset matches the schema from Airtable
+// still may need some inline parse-processing, hence the file and schema setup. 
+
+/**
+ * @param {*} record 
+ */
+const convertToAirtable = (record) => {
+    const result = Object.assign({}, schema);
+
+    for (field in record) {
+        result[field] = record[field]
+    }
+    return result;
+}
+
+module.exports = {
+    convertToAirtable
+}
