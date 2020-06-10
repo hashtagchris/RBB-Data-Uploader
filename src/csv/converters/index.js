f4d2d2f611ed4b4e9d25cc9e95508e091ae62d06
Jayson J. Phillips <58239+jaysonjphillips@users.noreply.github.com>
2020-06-09 19:55:29 -0700

    feat: 🎸 Support for Ranktribe Data Set (#4)
    Added Adapter and conversion mapping for the ranktribe dataset
    
    Co-authored-by: Jayson J. Phillips <jjp@breakpointt.studio>

--- src/csv/converters/index.js
+++ src/csv/converters/index.js
@@ -1,9 +1,11 @@
 const dineblack = require('./dineblack');
 const gofundme = require('./gofundme');
 const obws = require('./obws');
+const ranktribe = require('./ranktribe');
 
 module.exports = {
     dineblack,
     gofundme,
-    obws
+    obws,
+    ranktribe
 }
